b692e336dbf5ec927c108abe5211b0938d2d6916
German Dieser <germandieser@gmail.com>
2024-05-02 00:00:06 -0300

    Guia While

---
+++ GUIA 3 - C# CICLOS WHILE/3 - C# CICLOS WHILE/Guia3 While/Guia3 While/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Guia3_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
